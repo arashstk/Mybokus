c3051a330d5b3939b9b676d7493da7b475ff71ea
unknown <arash.nouri@chasacademy.se>
2020-11-05 22:42:17 +0100

    adding step1 by arash

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,44 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $books = [
+            'Red',
+            'Blue',
+            'Green',
+            'Yellow',
+            'Black',
+            'White',
+            'Magenta',
+            'Orange',
+            'Grey',
+            'Brown'   
+            
+        ];
+
+        foreach ($colors as $color) {
+            $c = new \App\Models\Color();
+            $c->title = $color;
+            $c->save();
+        }
+
+
+        $makes = [
+            'Toyota',
+            'Peugeot',
+            'Suzuki',
+            'Fiat',
+            'Honda',
+            'Ford',
+            'Hyundai',
+            'Renault',
+            'Volskwagen',
+            'Chrystler'
+        ];
+
+        foreach ($makes as $make) {
+            $c = new \App\Models\Make();
+            $c->title = $make;
+            $c->save();
+        }
     }
 }
